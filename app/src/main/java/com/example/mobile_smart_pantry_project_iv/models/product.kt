e01302e2a76256ff8db57065f670dce608ece11d
fedderli <pokrzywnickikarol@gmail.com>
2026-03-09 15:09:34 +0100

    created model for produkts

---
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/models/product.kt
@@ -0,0 +1,9 @@
+package com.example.mobile_smart_pantry_project_iv.models
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val quantity: Int,
+    val unit: String,
+    val genre: String
+)
